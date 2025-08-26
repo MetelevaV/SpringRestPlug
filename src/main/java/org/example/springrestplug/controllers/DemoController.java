221503aabf874d11159160f9db3c815f33c7def9
Varvara Meteleva <meteleva.v2016@yandex.ru>
2025-08-26 22:00:37 +0300

    add delay

--- src/main/java/org/example/springrestplug/controllers/DemoController.java
+++ src/main/java/org/example/springrestplug/controllers/DemoController.java
@@ -9,14 +9,24 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class DemoController {
 
+    private void addDelay() {
+        try {
+            Thread.sleep(1000 + (int)(Math.random() * 1001));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
-    @GetMapping("/getLogin")
+
+    @GetMapping("/getData")
     public String returnLogin() {
+        addDelay();
         return "{\"login\":\"Login1\",\"status\":\"ok\"}";
     }
 
     @PostMapping(value = "/postData", consumes = "application/json", produces = "application/json")
     public ResponseData postData(@RequestBody ResponseData data) {
+        addDelay();
         LocalDateTime date = LocalDateTime.now();
         String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         return new ResponseData(data.getLogin(), data.getPassword(), formattedDate);
