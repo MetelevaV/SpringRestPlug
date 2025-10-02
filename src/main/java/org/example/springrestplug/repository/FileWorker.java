5bfbb08db3c912fdcbd0329620e7542d39e02292
MetelevaV <meteleva.v2015@yandex.ru>
2025-10-02 17:37:25 +0300

    feat: change methods and remove file

--- src/main/java/org/example/springrestplug/repository/FileWorker.java
+++ src/main/java/org/example/springrestplug/repository/FileWorker.java
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.example.springrestplug.model.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -19,13 +21,12 @@ import java.util.List;
 import java.util.Random;
 
 @Component
+@Service
 public class FileWorker {
-    @Value("${app.file1.path}")
-    private String file1Path;
-    @Value("${app.file2.path}")
-    private String file2Path;
-    @Value("${app.file3.path}")
-    private String csvFilePath;
+    @Value("${app.file.path}")
+    private String filePath;
+    @Value("classpath:randomUsers.txt")
+    private Resource resource;
     private final ObjectMapper objectMapper;
     private final Random random = new Random();
 
@@ -33,47 +34,29 @@ public class FileWorker {
         this.objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
     public void writeUserToFile(User user) throws IOException {
-        File file = new File(file1Path);
-
-        List<User> users = new ArrayList<>();
-
-        if (file.exists() && file.length() > 0) {
-            users = objectMapper.readValue(file, new TypeReference<List<User>>() {});
-        }
-
-        users.add(user);
-
-        objectMapper.writeValue(file, users);
-    }
-
-    public void writeUserToCsv(User user) throws IOException {
-        try (FileWriter writer = new FileWriter(csvFilePath, true)) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-            String line = String.join(",",
-                    user.getLogin(),
-                    user.getEmail(),
-                    user.getPassword(),
-                    user.getDate().format(formatter)
-            );
-
-            writer.write(line + System.lineSeparator());
+        try (FileWriter fw = new FileWriter(filePath, true);) {
+            fw.write(objectMapper.writeValueAsString(user));
+            fw.write(System.lineSeparator());
         }
     }
 
     public String readRandomFromFile() throws IOException {
-        JsonNode root = objectMapper.readTree(Paths.get(file2Path).toFile());
+        int targetLine = random.nextInt(10);
 
-        if (!root.isArray() || root.isEmpty()) {
-            throw new IOException("Файл пуст или не содержит массив JSON");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(resource.getInputStream()))) {
+
+            for (int i = 0; i <= targetLine; i++) {
+                String line = reader.readLine();
+                if (i == targetLine) {
+                    return objectMapper.writeValueAsString(line);
+                }
+            }
         }
-        JsonNode randomNode = root.get(random.nextInt(root.size()));
-
-        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(randomNode);
+        throw new IOException("Файл пустой или произошла ошибка при чтении");
     }
 }
 
