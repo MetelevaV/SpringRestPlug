f7b2b1b1fd7d196c1f31f2a7bcccd0232a08d537
MetelevaV <meteleva.v2015@yandex.ru>
2025-10-03 10:19:20 +0300

    feat: change methods

--- src/main/java/org/example/springrestplug/repository/FileWorker.java
+++ src/main/java/org/example/springrestplug/repository/FileWorker.java
@@ -1,26 +1,18 @@
 package org.example.springrestplug.repository;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.example.springrestplug.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-@Component
+
 @Service
 public class FileWorker {
     @Value("${app.file.path}")
@@ -47,7 +39,7 @@ public class FileWorker {
         int targetLine = random.nextInt(10);
 
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(resource.getInputStream()))) {
+                new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
 
             for (int i = 0; i <= targetLine; i++) {
                 String line = reader.readLine();
